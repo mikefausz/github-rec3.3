3b3fcd51a0c277ffd6f791e9bacea843e62fe28e
Michael Fausz <mikefausz@Michaels-MacBook-Pro.local>
2016-02-18 23:09:45 -0500

    Added hrefs and urls via jQuery, tweaked link hovers in css.

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var setUserHtml = function() {
+    $('aside').prepend("<a href='" + userData.html_url + "'><img class='avatar' src='" + userData.avatar_url + "' alt='' /></a>");
     $('.name-box').find('h1').text(userData.name);
     $('.name-box').find('h2').text(userData.login);
     $('aside').find('.location').text(userData.location);
     $('aside').find('.date-joined').text("Joined on " + moment(userData.created_at).format('MMM D, YYYY'));
-    $('.stat-box').find('.followers').text(userData.followers);
-    $('.stat-box').find('.starred').text("0");
-    $('.stat-box').find('.following').text(userData.following);
+    var userStatHtml = "<a href='" + userData.followers_url + "'class='stat'>"
+                      + "<div class='stat-num followers'>" + userData.followers + "</div>"
+                      + "<div class='stat-name'>Followers</div>"
+                    + "</a>"
+                    + "<a href='" + userData.starred_url + "'class='stat'>"
+                      + "<div class='stat-num followers'>" + "0" + "</div>"
+                      + "<div class='stat-name'>Followers</div>"
+                    + "</a>"
+                    + "<a href='" + userData.following_url + "'class='stat'>"
+                      + "<div class='stat-num followers'>" + userData.following + "</div>"
+                      + "<div class='stat-name'>Followers</div>"
+                    + "</a>"
+    $('.stat-box').html(userStatHtml);
 }
 
 var sortReposByUpdate = function(rawRepos) {
@@ -15,14 +26,18 @@ var sortReposByUpdate = function(rawRepos) {
 var setRepoHtml = function(repoData) {
   var repoHtml = "";
   repoData.forEach(function(repo) {
-    repoHtml += "<div class='repo-list'>";
-    repoHtml += "<div class='repo-stats'>";
-    repoHtml += "<span class='repo-lang'>" + repo.language + "</span>";
-    repoHtml += "<a href='#' class='repo-stat-item'><span class='octicon octicon-star'></span>" + repo.stargazers_count + "</a>";
-    repoHtml += "<a href='#' class='repo-stat-item'><span class='octicon octicon-git-branch'></span>" + repo.forks_count + "</a></div>";
-    repoHtml += "<h3 class='repo-list-name'>" + repo.name + "</h3>";
-    repoHtml += "<p class='repo-desc'>" + repo.description + "</p>";
-    repoHtml += "<p class='repo-last-up'>Updated " + moment(repo.updated_at, "YYYYMMDDH").fromNow() + "</p></div>";
+    repoHtml += "<div class='repo-list'>"
+                + "<div class='repo-stats'>"
+                  + "<span class='repo-lang'>"
+                    + repo.language
+                  + "</span>"
+                  + "<a href='" + repo.stargazers_url + "' class='repo-stat-item'><span class='octicon octicon-star'></span>" + repo.stargazers_count + "</a>"
+                  + "<a href='" + repo.forks_url + "' class='repo-stat-item'><span class='octicon octicon-git-branch'></span>" + repo.forks_count + "</a>"
+                + "</div>"
+                + "<a href='" + repo.html_url + "'><h3 class='repo-list-name'>" + repo.name + "</h3></a>"
+                + "<p class='repo-desc'>" + repo.description + "</p>"
+                + "<p class='repo-last-up'>Updated " + moment(repo.updated_at, "YYYYMMDDH").fromNow() + "</p>"
+              + "</div>";
   });
   $('.repo-box').html(repoHtml);
 };
@@ -33,27 +48,31 @@ var setActivityHtml = function(activityData) {
     if (activityList.type === "CreateEvent") {
       activityHtml += "<div class='activity-list border-top'>";
       activityHtml += "<div class='activity-text'>";
-      activityHtml += "<span class='username'>" + activityList.actor.login + "</span> created ";
+      activityHtml += "<a href='" + userData.html_url + "'><span class='username'>" + activityList.actor.login + "</span></a> created ";
       if (activityList.payload.ref_type === "branch") {
-        activityHtml += "branch <span class='branch-name'>" + activityList.payload.ref +  "</span> at <span class='file-path'>" + activityList.repo.name + "</span> <span class='time-ago'>" + moment(activityList.created_at, "YYYYMMDDH").fromNow() + "</span>";
+        activityHtml += "branch <a href='#'><span class='branch-name'>" + activityList.payload.ref +  "</span></a> at <a href='" + activityList.repo.url + "'><span class='file-path'>" + activityList.repo.name + "</span></a> <span class='time-ago'>" + moment(activityList.created_at, "YYYYMMDDH").fromNow() + "</span>";
         activityHtml += "<span class='octicon octicon-git-branch activity-icon'></span></div></div>";
       }
       else {
-        activityHtml += "repository at <span class='file-path'>" + activityList.repo.name + "</span> <span class='time-ago'>" + moment(activityList.created_at, "YYYYMMDDH").fromNow() + "</span>";
+        activityHtml += "repository at <a href='" + activityList.repo.url + "'><span class='file-path'>" + activityList.repo.name + "</span></a> <span class='time-ago'>" + moment(activityList.created_at, "YYYYMMDDH").fromNow() + "</span>";
         activityHtml += "<span class='octicon octicon-repo activity-icon'></span></div></div>";
       }
     }
     else {
-      activityHtml += "<div class='activity-list border-top push'>";
-      activityHtml += "<div class='activity-text'>";
-      activityHtml += "<span class='time-ago'>" + moment(activityList.created_at, "YYYYMMDDH").fromNow() + " ago</span>";
-      activityHtml += "<div class='title'>";
-      activityHtml += "<span class='username'>" + activityList.actor.login + "</span> pushed to <span class='branch-name'>master</span> at <span class='file-path'>" + activityList.repo.name + "</span></div>";
-      activityHtml += "<div class='details'>";
-      activityHtml += "<img class='avatar' src='" + activityList.actor.avatar_url + "' alt=''/>";
-      activityHtml += "<span class='octicon octicon-mark-github'></span>";
-      activityHtml += "<span class='head'>e0bde6e</span>" + activityList.payload.commits[0].message + "</div>";
-      activityHtml += "<span class='mega-octicon octicon-git-commit activity-icon'></span></div></div>";
+      activityHtml += "<div class='activity-list border-top push'>"
+                      + "<div class='activity-text'>"
+                        + "<span class='time-ago'>" + moment(activityList.created_at, "YYYYMMDDH").fromNow() + " ago</span>"
+                        + "<div class='title'>"
+                          + "<a href='" + userData.html_url + "'><span class='username'>" + activityList.actor.login + "</span></a> pushed to <a href='#'><span class='branch-name'>master</span></a> at <a href='" + activityList.repo.url + "'><span class='file-path'>" + activityList.repo.name + "</span></a>"
+                        + "</div>"
+                        + "<div class='details'>"
+                          + "<a href='" + userData.html_url + "'><img class='avatar' src='" + activityList.actor.avatar_url + "' alt=''/></a>"
+                          + "<span class='octicon octicon-mark-github'></span>"
+                          + "<a href='#' class='head'>e0bde6e</a>" + activityList.payload.commits[0].message
+                        + "</div>"
+                        + "<span class='mega-octicon octicon-git-commit activity-icon'></span>"
+                      + "</div>"
+                    + "</div>";
     }
   });
   $('.activity-feed').append(activityHtml);
